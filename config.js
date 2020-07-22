12877c59042ead023ab448770ecd13e2d8b89506
Claire Hendry <claire.d.hendry@gmail.com>
2020-07-22 11:58:20 -0400

    initial deploy test

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
-    CLIENT_ORIGIN: "http://localhost:3000/",
+    CLIENT_ORIGIN: "http://localhost:3000/" || "https://creatives-block-client.vercel.app/",
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://CreativesBlock:password@localhost/CreativesBlock-test',
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://CreativesBlock:password@localhost/CreativesBlock',
     TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://CreativesBlock:password@localhost/CreativesBlock-test',
     API_TOKEN: process.env.API_TOKEN,
     API_KEY: process.env.API_KEY,
