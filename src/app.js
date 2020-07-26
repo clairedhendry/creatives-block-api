23e04aff6617c5c91ad50d17cca706d1530ec56a
Claire Hendry <claire.d.hendry@gmail.com>
2020-07-26 13:57:28 -0400

    cors errors on upload

--- src/app.js
+++ src/app.js
@@ -18,11 +18,11 @@ const morganOption = NODE_ENV === "production" ? "tiny" : "common";
 app.use(morgan(morganOption));
 app.use(helmet());
 app.options("*", cors());
-// app.use(
-//   cors({
-//     origin: CLIENT_ORIGIN,
-//   })
-// );
+app.use(
+  cors({
+    origin: CLIENT_ORIGIN,
+  })
+);
 // app.use(cors());
 
 app.use("/api/recent-blocks", UserBlocksRouter);
